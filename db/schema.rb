77e2ac00e4023e996f250b326d1b57842033646b
danutibe07 <danielutibe07@gmail.com>
2023-06-12 00:01:23 +0100

    Added comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_11_224346) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_225630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
