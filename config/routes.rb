40c2260ade6df0257cd395b4da51637c29b54848
danutibe07 <danielutibe07@gmail.com>
2023-06-15 22:25:59 +0100

    Defined routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@
 
 Rails.application.routes.draw do
   root "pages#blog"
+  resources :users, only: [:index, :show]
+  resources :posts, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Additional routes...
