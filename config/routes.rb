f6bd5867bb74ac5b24f14e1414a29368253b662f
danutibe07 <danielutibe07@gmail.com>
2023-06-16 04:02:28 +0100

    Controller and view for Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root "pages#blog"
+  root "users#index"
   resources :users, only: [:index, :show]
   resources :posts, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
