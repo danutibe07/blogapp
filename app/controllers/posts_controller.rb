402fea574134bcdb4d9a7bb30210b5e3dac7f797
danutibe07 <danielutibe07@gmail.com>
2023-06-19 09:24:19 +0100

    Updated rubocop errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,13 @@
-  class PostsController < ApplicationController
-    def index
-      # @users = User.all
-      # @user = @users.find(params[:user_id])
-      # @posts = @user.posts
-    end 
-  
-    def show
-      # @users = User.all
-      # @user = @users.find(params[:user_id])
-      # @post = @user.posts.find(params[:id])
-    end
+class PostsController < ApplicationController
+  def index
+    # @users = User.all
+    # @user = @users.find(params[:user_id])
+    # @posts = @user.posts
   end
+
+  def show
+    # @users = User.all
+    # @user = @users.find(params[:user_id])
+    # @post = @user.posts.find(params[:id])
+  end
+end
