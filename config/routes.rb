d223522f2b9fac514257b22c07f2bac0a3a6e7ca
danutibe07 <danielutibe07@gmail.com>
2023-06-22 02:34:50 +0100

    Created new comment view and contollerd

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,15 @@ Rails.application.routes.draw do
   root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :create, :new] do
+      resources :comments, only: [:create, :new]
+      post 'like', on: :member
+      # resources :likes, only: [:create]
   end
+end
+  resources :posts do
 
+  end
+  
   # Define your other application routes here
 end
